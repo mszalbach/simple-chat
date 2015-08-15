dabb02e5fcfc3619221f629fe2487660ccc0feac
mszalbach <marcel.szalbach@gmail.com>
2015-08-15 09:33:01 +0200

    Add Selenium Test #9:  - rewritten SeleniumDriver to normal POJO managed by guice  - added web annotation so only features with this start and stop a browser instance

--- chat-acceptancetests/src/main/java/de/blogspot/mszalbach/chat/selenium/SeleniumDriver.java
+++ chat-acceptancetests/src/main/java/de/blogspot/mszalbach/chat/selenium/SeleniumDriver.java
@@ -1,10 +1,12 @@
 package de.blogspot.mszalbach.chat.selenium;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.runtime.java.guice.ScenarioScoped;
 import de.blogspot.mszalbach.chat.Config;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.UnreachableBrowserException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.inject.Inject;
@@ -12,61 +14,44 @@ import javax.inject.Inject;
 /**
  * Created by foobar on 14.08.15.
  */
+@ScenarioScoped
 public class SeleniumDriver {
     private WebDriver webDriver;
     private Config config;
 
     @Inject
     SeleniumDriver(Config config) {
-
         this.config = config;
     }
 
 
-    public WebDriver getCurrentDriver() {
-        if (webDriver==null) {
-            try {
-                webDriver = config.getWebDriver();
-            } finally{
-                Runtime.getRuntime().addShutdownHook(
-                        new Thread(new BrowserCleanup()));
-            }
-        }
+    public WebDriver getDriver() {
         return webDriver;
     }
 
-
-    public  WebDriver getDriver() {
-        return getCurrentDriver();
+    @Before("@web")
+    public void openBrowser() {
+        webDriver = config.getWebDriver();
     }
 
-    private class BrowserCleanup implements Runnable {
-        public void run() {
-            close();
-        }
-    }
 
+    @After("@web")
     public void close() {
-        try {
-            getCurrentDriver().quit();
-            //without this the browser will not be closed
-            webDriver = null;
-        } catch (UnreachableBrowserException e) {
-        }
+        webDriver.quit();
     }
 
 
     public void loadPage(String url) {
-        getCurrentDriver().get("http://" + config.getApplicationURL() + "/" + config.getApplicationContext() + "/" + url);
+        webDriver.get("http://" + config.getApplicationURL() + "/" + config.getApplicationContext() + "/" + url);
     }
 
     public void waitUntilPageContainsId(final String id) throws InterruptedException {
-        new WebDriverWait(getCurrentDriver(), 10)
+        new WebDriverWait(webDriver, 10)
                 .until((WebDriver d) -> d.findElement(By.id(id)));
     }
 
     public void typeIntoField(String id, String value) {
-        WebElement element = getCurrentDriver().findElement(By.id(id));
+        WebElement element = webDriver.findElement(By.id(id));
         element.clear();
         element.sendKeys(value);
     }
--- chat-acceptancetests/src/main/resources/webchat.feature
+++ chat-acceptancetests/src/main/resources/webchat.feature
@@ -1,3 +1,4 @@
+@web
 Feature: As a website user I want to chat with others
 
   Scenario: Send message to myself
@@ -5,7 +6,6 @@ Feature: As a website user I want to chat with others
     When I change my nickname to "client1"
     When I send message "Hello"
     Then webchat contains message "client1 - Hello"
-
 
   Scenario: Send message to myself
     Given I visit the chat website
