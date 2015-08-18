f9fa27789516c6f7610e722908d5a552cc701e37
mszalbach <marcel.szalbach@gmail.com>
2015-08-18 14:58:23 +0200

    Provide a History for Messages #3:  - added mongodb to acceptancetest module

--- chat-acceptancetests/src/main/java/AllStories.java
+++ chat-acceptancetests/src/main/java/AllStories.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
  * Created by foobar on 15.05.15.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"}, tags = {"~@Ignore"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"},
+        tags = {"~@Ignore"} //so @Ignore can be used to disable Scenarios
+)
+
 public class AllStories {
 }
